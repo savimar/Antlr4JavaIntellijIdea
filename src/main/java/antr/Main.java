d557dd5f5c7b4cf198912fac5941c27068c870a4
savimar <sam409@yandex.ru>
2017-10-31 09:24:04 +0300

    fix img generate_recoqnizer

---
+++ src/main/java/antr/Main.java
@@ -0,0 +1,19 @@
+package antr;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class Main {
+    public static void main( String[] args) throws Exception
+    {
+        HelloLexer lexer = new HelloLexer(new ANTLRInputStream("hello world"));
+           //     ( new ANTLRFileStream/*(args[0])*/("")); //hello world
+        CommonTokenStream tokens = new CommonTokenStream( lexer );
+        HelloParser parser = new HelloParser( tokens );
+        ParseTree tree = parser.r();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk( new HelloWalker(), tree );
+    }
+}
